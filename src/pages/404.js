5127daf5190d3547164cf4099ba0fd571db4fdc8
Darcy LF <ehnevault@gmail.com>
2020-08-21 18:16:35 +1000

    fix up error copy

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,12 @@
 import React from "react"
 
 import Layout from "../components/layout"
-import {Box, Text, Heading} from "theme-ui"
+import {Box, Text, Heading, Link} from "theme-ui"
 import Head from "../components/Head"
+
+const CustomLink = (props) => (
+  <Link sx={{color:"background", pr:3, ":hover":{color:"background",  textDecoration:"none"}}} {...props}></Link>
+)
 
 const NotFoundPage = () => (
   <Layout>
@@ -10,7 +14,12 @@ const NotFoundPage = () => (
     <Box backgroundColor="primary" color="background" p={4}>
     <Heading sx={{fontSize:[6,7]}}>NOT FOUND</Heading>
 
-    <Text sx={{fontSize:3}}>You just found a URL that doesn't exist.</Text>
+    <Text sx={{fontSize:3}}>Error 404. Looks like that page doesn't exist.</Text>
+
+    <Text sx={{fontSize:3}}>
+      <CustomLink href="javascript:history.back()">go back</CustomLink>
+      <CustomLink href="/">go to collective-fullstack.github.io</CustomLink>
+    </Text>
     </Box>
    
   </Layout>
