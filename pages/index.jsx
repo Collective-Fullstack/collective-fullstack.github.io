3bd2c611c8a3ddde663f41e656b99904b99d641f
Darcy LF <ehnevault@gmail.com>
2020-11-17 21:21:22 +1100

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
 
-function HomePage() {
-  return (<div>Welcome to Next.js!</div>);
+import fs from 'fs';
+import path from 'path';
+
+function HomePage({ filenames }) {
+  return (
+    <div>
+      Welcome to Next.js!
+      {filenames}
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  // fetches the posts in the directory "work"
+  const postsDirectory = path.join(process.cwd(), 'work');
+  // because this stuff is done in the build step, we can use just normal node stuff.
+  const filenames = fs.readdirSync(postsDirectory);
+
+  // return the filenames as a prop
+  return {
+    props: {
+      filenames,
+    },
+  };
 }
 
 export default HomePage;
