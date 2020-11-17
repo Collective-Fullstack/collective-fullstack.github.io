1ede727d624c7962511eec972a6171d5945d18e8
Darcy LF <ehnevault@gmail.com>
2020-11-18 08:04:33 +1100

    add theme

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,12 +1,16 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import * as React from 'react';
+import { ChakraProvider, Box } from '@chakra-ui/react';
 
-import { ChakraProvider } from '@chakra-ui/react';
+import theme from '../lib/theme';
 
 const AppContainer = ({ Component, pageProps }) => (
-  <ChakraProvider>
-    <Component {...pageProps} />
+  <ChakraProvider theme={theme}>
+    <Box m={2}>
+      <Component {...pageProps} />
+    </Box>
   </ChakraProvider>
 );
-// Component is the entire pages component
+
+// Component is the entire page's component
 export default AppContainer;
